f64e1ab35806c6db645cb7bbbd320e28ee6ddb2c
Ricardo Labarca <rimpa007@hotmail.com>
2019-05-31 10:59:00 -0500

    delete unuseful functions

--- src/Components/Peticiones.js
+++ src/Components/Peticiones.js
@@ -12,22 +12,7 @@ class Peticiones extends React.Component{
 
     componentDidMount(){
       // though never re-rendered, the component will receive new props here
-      var callbacks = {};
 
-      function ie() {
-        return (navigator.appVersion.indexOf("MSIE 6") !== -1 || navigator.appVersion.indexOf("MSIE 7") !== -1 || navigator.appVersion.indexOf("MSIE 8") !== -1);
-      }
-      var execCallback = function(id) {
-        if (!callbacks[id])
-          return;
-        callbacks[id].call();
-      };
-
-      function wrapCallback(callback) {
-        var id = (new Date()).getTime();
-        callbacks[id] = callback;
-        return id;
-      }
       /// Fetch varable from 'GET' request
       function getHtmlVar(name) {
         if (!name)
@@ -42,33 +27,15 @@ class Peticiones extends React.Component{
         }
         return null;
       }
-      /// Load script
-      function loadScript(src, callback) {
-        var script = document.createElement("script");
-        script.setAttribute("type","text/javascript");
-        script.setAttribute("charset","UTF-8");
-        script.setAttribute("src", src);
-        if (callback && typeof callback === "function") {
-          wrapCallback(callback);
-          if (ie())
-            script.onreadystatechange = function () {
-              if (this.readyState === 'complete' || this.readyState === 'loaded')
-                callback();
-            };
-          else
-            script.setAttribute("window.onLoad", "window.execCallback(" + wrapCallback(callback) + ")");
-        }
-        document.getElementsByTagName("head")[0].appendChild(script);
-      }
 
       /// Login result
       function login(code) {
-        console.log( "login" + ": " + code );
+        console.log( "login: " + code );
         if (code) {
           alert( "Login error" );
           return;
         }
-        alert( "Hello" + ", " + window.wialon.core.Session.getInstance().getCurrUser().getName() + "!" );
+        alert( "Hello, " + window.wialon.core.Session.getInstance().getCurrUser().getName() + "!" );
       }
 
       /// Init SDK
@@ -79,24 +46,11 @@ class Peticiones extends React.Component{
         window.wialon.core.Session.getInstance().initSession(url);
         window.wialon.core.Session.getInstance().duplicate(getHtmlVar("sid"), user, true, login);
       }
-      /// We are ready now
-      function onLoad() {
-        // load translations
-        var lang = getHtmlVar("lang") || "en";
-        if (["en", "ru"].indexOf(lang) === -1){
-          lang = "en";
-        }
-        
-        // load wialon.js
-        var url = getHtmlVar("baseUrl") || getHtmlVar("hostUrl") || "https://hst-api.wialon.com";
-        loadScript(url+"/wsdk/script/wialon.js", initSdk);
-      }
-    //onLoad();
     initSdk();
   }
 
     render() {
-    return <div ref="hw" />;
+    return <div/>;
     }
 }
 
